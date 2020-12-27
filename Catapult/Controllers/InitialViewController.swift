51ba627f123c02e20dd3d135c60b427502b6acfc
alisonsoong <alisonsoong@gmail.com>
2020-12-26 21:27:05 -0800

    Initial Screen

---
+++ Catapult/Controllers/InitialViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InitialViewController.swift
+//  Catapult
+//
+//  Created by Alison Soong on 12/26/20.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    @IBOutlet weak var initialScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // scroll reset
+        initialScrollView.setContentOffset(CGPoint.zero, animated: true)
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
