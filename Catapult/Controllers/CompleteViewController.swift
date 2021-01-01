bfad8df5ab5cea5b90192c66022b9750e3a964c3
alisonsoong <alisonsoong@gmail.com>
2020-12-31 17:52:32 -0800

    Everything resets after done

--- Catapult/Controllers/CompleteViewController.swift
+++ Catapult/Controllers/CompleteViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class CompleteViewController: UIViewController {
 
+    let defaults = UserDefaults.standard
+    let addressLine1Key = "addressLine1"
+    let addressLine2Key = "addressLine2"
+    let cityKey = "city"
+    let stateKey = "state"
+    let postalKey = "postal"
+    let firstNameKey = "firstName"
+    let lastNameKey = "lastName"
+    let phoneKey = "phone"
+    let emailKey = "email"
+    let emailDomainKey = "emailDomain"
+    
     @IBOutlet weak var CloseButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +28,23 @@ class CompleteViewController: UIViewController {
         CloseButton.layer.cornerRadius = CloseButton.frame.size.height / 5
         
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        
+        // reset everything
+        self.defaults.set("", forKey: self.addressLine1Key)
+        self.defaults.set("", forKey: self.addressLine2Key)
+        self.defaults.set("", forKey: self.cityKey)
+        self.defaults.set("", forKey: self.stateKey)
+        self.defaults.set("", forKey: self.postalKey)
+        self.defaults.set("", forKey: self.firstNameKey)
+        self.defaults.set("", forKey: self.lastNameKey)
+        self.defaults.set("", forKey: self.phoneKey)
+        self.defaults.set("", forKey: self.emailKey)
+        
+    }
+    
     
 
     /*
