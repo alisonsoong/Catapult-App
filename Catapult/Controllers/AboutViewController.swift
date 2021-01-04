134ffdde2b79c45c6bb835c21a2347bf5d064e17
alisonsoong <alisonsoong@gmail.com>
2021-01-04 09:18:34 -0800

    Fixed settings infinite loop

---
+++ Catapult/Controllers/AboutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutViewController.swift
+//  Catapult
+//
+//  Created by Alison Soong on 1/4/21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
