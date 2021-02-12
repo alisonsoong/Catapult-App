dab60e4c0cde10507082dcfb2d9534bc091b147e
alisonsoong <alisonsoong@gmail.com>
2021-02-11 18:52:14 -0800

    formatting fixes
    Previous push also included improved error message (got rid of popups)

--- Catapult/Controllers/SettingsViewController.swift
+++ Catapult/Controllers/SettingsViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+    let screenKey = "startScreenIndex"
+    let addressLine1Key = "addressLine1"
+    let addressLine2Key = "addressLine2"
+    let cityKey = "city"
+    let stateKey = "state"
+    let postalKey = "postal"
+    let firstNameKey = "firstName"
+    let lastNameKey = "lastName"
+    let phoneKey = "phone"
+    let emailKey = "email"
+    let emailDomainKey = "emailDomain"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +33,24 @@ class SettingsViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func restartButtonPressed(_ sender: UIButton) {
+        
+        // reset everything
+        self.defaults.set("", forKey: self.addressLine1Key)
+        self.defaults.set("", forKey: self.addressLine2Key)
+        self.defaults.set("", forKey: self.cityKey)
+        self.defaults.set("", forKey: self.stateKey)
+        self.defaults.set("", forKey: self.postalKey)
+        self.defaults.set("", forKey: self.firstNameKey)
+        self.defaults.set("", forKey: self.lastNameKey)
+        self.defaults.set("", forKey: self.phoneKey)
+        self.defaults.set("", forKey: self.emailKey)
+        
+        // go back to original screen
+        self.defaults.set(0, forKey: self.screenKey)
+        self.dismiss(animated: true, completion: nil)
+        
+    }
     /*
     // MARK: - Navigation
 
